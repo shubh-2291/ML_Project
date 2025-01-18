a48806c0fb7361e508d70f906356fdf4a783488b
shubham sharma <iamshubhamsharma375@gmail.com>
2025-01-18 18:44:12 +0530

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "ML_Project"
+AUTHOR_USER_NAME = "shubh-2291"
+AUTHOR_EMAIL = "iamshubhamsharma375@gmail.com"
+setuptools.setup(
+    name=REPO_NAME,
+    version= __version__,
+    author= AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/shubh-2291/ML_Project",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
