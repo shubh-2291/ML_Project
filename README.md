20e0ab14d5a94e2f20a936522ef4f4edbc2f5c54
shubham sharma <iamshubhamsharma375@gmail.com>
2025-02-24 10:55:48 +0530

    Ml flow added

--- README.md
+++ README.md
@@ -11,3 +11,65 @@
 7. Update the pipeline
 8. Update the main.py
 9. Update the app.py
+
+# How to run?
+
+### STEPS:
+ Clone the repository
+
+ ```bash
+https://github.com/shubh-2291/ML_Project.git
+```
+### STEP 01- Create a virtual environment after opening the repository
+
+```bash
+virtualenv env
+```
+#### Note: if virtualenv not installed , intalled it.
+```bash
+pip install virtualenv
+```
+### step 02- activate virtual environment
+```bash
+env/Scripts/activate
+```
+### step 03- install the requirements
+```bash
+pip install -r requirements.txt
+```
+```bash
+python app.py
+```
+Now,
+``` bash
+open up your host and port
+```
+
+## MLflow
+
+[Documentation](https://mlflow.org/docs/latest/index.html)
+
+#### cmd
+- mlflow ui
+
+### dagshub
+[dagshub](https://dagshub.com)
+
+MLFLOW_TRACKING_URI=[https://dagshub.com/entbappy/End-to-end-Machine-Learning-Project-with-MLflow.mlflow \](https://dagshub.com/shubh-2291/ML_Project.mlflow)
+
+MLFLOW_TRACKING_USERNAME=shubh-2291
+
+MLFLOW_TRACKING_PASSWORD=bda8667039f829190ef0956963cf5992817e71f0
+python script.py
+
+Run this to export as env variables:
+
+```bash
+
+set MLFLOW_TRACKING_URI=https://dagshub.com/shubh-2291/ML_Project.mlflow
+
+set MLFLOW_TRACKING_USERNAME=shubh-2291
+
+set MLFLOW_TRACKING_PASSWORD=bda8667039f829190ef0956963cf5992817e71f0
+
+```
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from ML_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPip
 from ML_Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from ML_Project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from ML_Project.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from ML_Project.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
@@ -17,7 +17,6 @@ except Exception as e:
 
 
 STAGE_NAME = "Data validation stage"
-
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     obj = DataValidationTrainingPipeline()
@@ -29,7 +28,6 @@ except Exception as e:
 
 
 STAGE_NAME = "Data Transformation stage"
-
 try:
     logger.info(f">>>>> stage {STAGE_NAME} Started <<<<<")
     obj = DataTransformationTrainingPipeline()
@@ -40,10 +38,19 @@ except Exception as e:
     raise e
 
 STAGE_NAME = "Model Trainer stage"
-
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model evaluation stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = ModelEvaluationTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
 except Exception as e:
